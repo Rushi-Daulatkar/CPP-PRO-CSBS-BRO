7459a725ffd93a8f936985c3d9e06c58ae5d2f88
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-18 11:58:38 +0530

    Taking Value From User.cpp
    Taking Value From User.cpp

---
+++ Taking Value From User.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int a;
+    cout << "Enter any Number" << endl;
+    cin >> a;
+    cout << "The value of  A = " << a;
+
+    return 0;
+}
