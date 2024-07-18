3b1eae2f3990c2758c15d9cff5bcc0781b61f898
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-18 11:55:41 +0530

    Output  Screen.cpp
    Output  Screen.cpp

---
+++ Output  Screen.cpp
@@ -0,0 +1,9 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    cout << "To print Something on O/P Screen";
+
+    return 0;
+}
