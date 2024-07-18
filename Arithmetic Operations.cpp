10533a1e6553018133fe4478659f94df6ac541e7
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-18 12:11:10 +0530

    Arithmetic Operations.cpp
    Arithmetic Operations.cpp

---
+++ Arithmetic Operations.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a,b;
+    cout << "Enter any two Numbers : " << endl;
+    cin >> a >>b;
+    cout << a << " + " << b << " = " << a+b << endl;
+    cout << a << " - " << b << " = " << a-b << endl;
+    cout << a << " * " << b << " = " << a*b << endl;
+    cout << a << " / " << b << " = " << a/b << endl;
+    
+    return 0;
+}
