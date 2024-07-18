52fc2853cedda980d3ce1aaac1bd190be3b28928
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-18 12:03:27 +0530

    Tab.cpp
    Tab.cpp

---
+++ Tab.cpp
@@ -0,0 +1,8 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    cout << "C \n\tC++\n\t\tJava";
+    return 0;
+}
