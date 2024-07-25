f9c8c92dc94b42457312fe31d951de7292c2f62b
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-25 12:01:49 +0530

    Perimeter of Rectangle.cpp
    Perimeter of Rectangle

---
+++ Perimeter of Rectangle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float l , w;
+    
+    cout << "Enter the value of Length and Width of Rectangle :" << endl;
+    cin >> l >> w;
+    cout << "The Perimeter of Rectangle is : "<<  2*(l * w) << " units" << endl;
+}
