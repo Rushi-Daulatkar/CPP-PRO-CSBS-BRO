3d01b5d53bc8aca591d049a37360e79fef513762
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-25 11:59:09 +0530

    Circumference of Circle.cpp
    Circumference of Circle

---
+++ Circumference of Circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float radius;
+    float pi = 3.142;
+    cout << "Enter the value of Radius :" << endl;
+    cin >> radius;
+    cout << "The Circumference of Circle is : "<<  2*pi*radius << " sq.fts" << endl;
+}
