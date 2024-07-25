9f50a04a3c6de20737bfddc1c0e75540e16623d6
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-25 11:53:09 +0530

    Area of Rectangle.cpp

---
+++ Area of Rectangle.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float l , b;
+    cout << "Enter any Two Numbers :" << endl;
+    cin >> l >> b;
+    cout << "The Area of Rectangle is :"<<  l*b << " sq.fts" << endl;
+}
