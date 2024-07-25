ad10d030ea2424b19c1ae59317a61b720daf86d2
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-25 11:55:57 +0530

    Area of Circle.cpp
    Area of Circle

---
+++ Area of Circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float radius;
+    float pi = 3.142;
+    cout << "Enter the value of Radius :" << endl;
+    cin >> radius;
+    cout << "The Area of Circle is : "<<  pi*(radius * radius) << " sq.fts" << endl;
+}
