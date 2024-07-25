d71f23131a66d8d21da6d3519aad89f06b20abf6
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-07-25 11:43:58 +0530

    Average of 4 Numbers.cpp
    Average of 4 Numbers.cpp

---
+++ Average of 4 Numbers.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float a , b , c , d;
+    cout << "Enter any Four Numbers :" << endl;
+    cin >> a >> b >> c >> d;
+    cout << "The Avg of is :" << (a+b+c+d) / 4 << endl;
